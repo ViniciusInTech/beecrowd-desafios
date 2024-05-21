43fc0b64b509863f7672c781d98dcdf7be675948
viniciusintech <vini989073599@gmail.com>
2024-05-21 18:44:55 -0300

    feat: 1008-Salary

---
+++ salary/salary.py
@@ -0,0 +1,8 @@
+employee = int(input())
+
+hours = int(input())
+receive_per_hour = float(input())
+
+salary = hours * receive_per_hour
+print(f"NUMBER = {employee}")
+print(f"SALARY = U$ {salary:.2f}")
