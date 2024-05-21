4688602f20bc10f1f76724f14f1ed13d1a50f90f
viniciusintech <vini989073599@gmail.com>
2024-05-21 13:33:56 -0300

    feat: average2

---
+++ average2/average2.py
@@ -0,0 +1,12 @@
+a = float(input())
+a_peso = 2
+
+b = float(input())
+b_peso = 3
+
+c = float(input())
+c_peso = 5
+
+media = ((a*a_peso) + (b*b_peso) + (c*c_peso)) / (a_peso+b_peso+c_peso)
+
+print(f"MEDIA = {media:.1f}")
