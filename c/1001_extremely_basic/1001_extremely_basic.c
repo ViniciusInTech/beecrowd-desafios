2d40da0afde8500d3595311a7e35b890fba83138
viniciusintech <vini989073599@gmail.com>
2025-04-29 22:46:57 -0300

    feat: 1001_extremely_basic

---
+++ c/1001_extremely_basic/1001_extremely_basic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int number_1, number_2;
+
+    scanf("%d", &number_1);
+    scanf("%d", &number_2);
+
+    printf("X = %d\n", number_1 + number_2);
+    return 0;
+}
